51fd9c08087f3cbf80db63de8149e9daee54b4fb
andrea singh <info@madebydna.com>
2010-10-27 15:32:13 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epistola::Application.config.secret_token = 'e399d1b42ea03509807a05df85026d7a782f79cef704217479225979bcafab3c30e297fc88ba1c3145bb19275da32cfec8f1e7c129f153f8e5680a3b62fc8c31'
